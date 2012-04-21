820d2b4cf69ea204b65eca532c8e87527b4fb44d
Sundar <mail@devsundar.com>
2012-04-21 15:23:20 +0530

    Food working, finally :) Workaround done for a bug in DART

--- dart/snake.dart
+++ dart/snake.dart
@@ -1,7 +1,6 @@
 #import('dart:html');
 
 class snake {
-  
   CanvasRenderingContext2D ctx;
   CanvasElement canvas = null;
   int x = 0;
@@ -9,6 +8,9 @@ class snake {
   int gridSize = 10;
   String direction = "right";
   var snakeBody = null;
+  int snakeLength = 3;
+  var xrand = 0.0, yrand = 0.0;
+  int randCount = 1;
   
   snake() {
     snakeBody = [];
@@ -23,10 +25,10 @@ class snake {
     var width = 10;
     var height = 10;
      
-    // This draws a square with the parameters from the variables set above
-    ctx.fillRect(xRect, yRect, width, height);
     document.on.keyDown.add(handleEvent);
     window.setInterval(moveSnake, 100);
+    this.makeFood();
+    this.drawSnake();
   }
   
   void handleEvent(event) {
@@ -60,10 +62,15 @@ class snake {
     this.snakeBody.add(coord);
     this.ctx.fillStyle = "rgb(200,0,0)";
     this.ctx.fillRect(this.x, this.y, this.gridSize, this.gridSize); 
-    if (this.snakeBody.length > 3) {
+    if (this.snakeBody.length > this.snakeLength) {
       var itemToRemove = this.snakeBody[0];
       this.snakeBody.removeRange(0, 1);
       this.ctx.clearRect(itemToRemove[0], itemToRemove[1], this.gridSize, this.gridSize);
+    }
+    print("x=" + this.x + "y=" + this.y + "xrand=" + this.xrand + "yrand=" + this.yrand);
+    if (this.x == this.xrand && this.y == this.yrand) {
+      this.makeFood();
+      this.snakeLength = this.snakeLength + 1;
     }
   }
   
@@ -152,11 +159,24 @@ class snake {
   }
   
   void makeFood() {
-    var xrand = 0, yrand = 0;
-    xrand = (Math.random() * (this.canvas.width / this.gridSize)).floor();
-    yrand = (Math.random() * (this.canvas.height / this.gridSize)).floor();
+    double rem = 0.0;
+    this.xrand = (this.getRandom() * (this.canvas.width)).floor();
+    this.yrand = (this.getRandom() * (this.canvas.height)).floor();
+    rem = this.xrand % this.gridSize;
+    this.xrand = this.xrand - rem;
+    rem = this.yrand % this.gridSize;
+    this.yrand = this.yrand - rem;
     this.ctx.fillStyle = "rgb(10,100,0)";
     this.ctx.fillRect(xrand, yrand, this.gridSize, this.gridSize);
+  }
+  
+  double getRandom() {
+    double rand = 0.0;
+    for (int i = 0; i < this.randCount; i++) {
+      rand = Math.random();
+    }
+    this.randCount = this.randCount + 1;
+    return rand;
   }
   
 }
